659ae5afe4f48aeb2774b1f31546440bfdd72f54
sure-660322 <1824263093@qq.com>
2025-12-01 22:05:29 +0800

    Add files via upload

--- cpp-firstTest.cpp
+++ cpp-firstTest.cpp
@@ -6,8 +6,10 @@ using namespace std;
 int main()
 {
 	string name = "sure";
+	string student_ID = "19240211";
 	int age = 19;
 	cout << "name:" << name << endl;
+	cout << "student_ID:" << student_ID << endl;
 	cout << "age:" << age << endl;
 	//2025-12-1
 }

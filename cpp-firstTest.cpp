9c8c8406d3646382ab10f3bea90ed29bb6b628d2
sure-660322 <1824263093@qq.com>
2025-12-01 22:00:25 +0800

    Add files via upload

---
+++ cpp-firstTest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string name = "sure";
+	int age = 19;
+	cout << "name:" << name << endl;
+	cout << "age:" << age << endl;
+	//2025-12-1
+}
